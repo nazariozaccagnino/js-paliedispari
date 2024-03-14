34e65d3a6919b16c71dff794aed7a00e228c4f15
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-03-14 15:05:27 +0100

    calculations done

--- palindroma/script.js
+++ palindroma/script.js
@@ -1,15 +1,16 @@
 let userWord = prompt('Inserisci una parola');
 let text = userWord.charAt(userWord.length - 1)
-console.log(text, 'ultima lettera della parola utente')
+// console.log(text, 'ultima lettera della parola utente')
 let oppositeWord = '';
 
 for(let i = userWord.length - 1; i >= 0; i--){
     // oppositeWord.push(userWord[i]);
     // console.log(userWord[i])
-    oppositeWord += userWord[i]
-    if( userWord === oppositeWord)
-        console.log('la parola è palindroma')
-    
-    
-   
-}console.log(oppositeWord);
+    oppositeWord += userWord[i]   
+}
+if(userWord === oppositeWord){
+    console.log('la parola è palindroma');
+}else{
+    console.log('la parola NON è palindroma')
+}
+// console.log(oppositeWord);
