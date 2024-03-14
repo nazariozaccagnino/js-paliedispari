6d57e5ebda750728df12f79ba80ac59f13e5c577
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-03-14 14:36:55 +0100

    trying calculations of palindroma exercise

---
+++ palindroma/script.js
@@ -0,0 +1,12 @@
+let userWord = prompt('Inserisci una parola');
+let text = userWord.charAt(userWord.length)
+console.log(text, 'prova')
+let oppositeWord = '';
+for(let i = userWord.length - 1; i >= 0; i--){
+    oppositeWord = i;
+    console
+    
+   
+}
+
+console.log(wordToCompare)
