bb92ddfd388743315220fb94daa21dc1f2eb6d51
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-03-14 16:31:50 +0100

    game almost functioning

--- pari_dispari/script.js
+++ pari_dispari/script.js
@@ -1,3 +1,8 @@
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
 
 let button = document.getElementById("button")
 button.addEventListener('click', function() {
@@ -6,9 +11,27 @@ button.addEventListener('click', function() {
 // console.log(odd)
 // let even = document.getElementById("even").value;//pari
 // console.log(even)
-let choice = document.getElementById("choice").value
-console.log(choice)
-let userNumber = document.getElementById("userNumber").value
-console.log(userNumber)
+let choice = parseInt(document.getElementById("choice").value);
+console.log(choice, 'pari o dispari');
+let userNumber = parseInt(document.getElementById("userNumber").value);
+console.log(userNumber, 'numero utente');
+let message = '';
+
+pcNumber = getRndInteger(1,5);
+console.log(pcNumber, 'numero pc casuale')
+result = pcNumber + userNumber;
+console.log(result, 'risultato della somma')
+
+if(choice === 1 && result % 2 === 0){
+    message = `pari, hai vinto`
+    console.log(message)
+}if(choice === 2 && result % 2 != 0){
+    message = `dispari, hai vinto`
+    console.log(message)
+}
 })
+
+
+
+
 
