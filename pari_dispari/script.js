70417728213fa650f691324c308b12921bf0a2cf
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-03-14 16:18:03 +0100

    created the user input and acquiring data

---
+++ pari_dispari/script.js
@@ -0,0 +1,14 @@
+
+let button = document.getElementById("button")
+button.addEventListener('click', function() {
+
+// let odd = document.getElementById("odd").value;//dispari
+// console.log(odd)
+// let even = document.getElementById("even").value;//pari
+// console.log(even)
+let choice = document.getElementById("choice").value
+console.log(choice)
+let userNumber = document.getElementById("userNumber").value
+console.log(userNumber)
+})
+
